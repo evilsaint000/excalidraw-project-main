a5c2d3b042f5a8f748d2a5d307586e470b661099
petergriffin29 <rishirajjsr20@gmail.com>
2025-08-17 12:51:30 +0530

    fixing deployment 2

--- hooks/use-drawing-tool.tsx
+++ hooks/use-drawing-tool.tsx
@@ -2,6 +2,10 @@
 
 import { ShapeType } from "@/types";
 import { useCanvasState } from "./use-canvas-state";
+import rough from "roughjs";
+import { RoughCanvas } from "roughjs/bin/canvas";
+import { Drawable } from "roughjs/bin/core";
+// drawOnCanvas is used for rendering; here we create element objects instead
 import { drawOnCanvas } from "@/lib/utils";
 
 export const useDrawingTool = (shapeType: ShapeType) => {
@@ -9,13 +13,24 @@ export const useDrawingTool = (shapeType: ShapeType) => {
 
     const handleMouseDown = (e: React.MouseEvent) => {
         const { x, y } = getScaledCoordinates(e.clientX, e.clientY);
-        const newElement = drawOnCanvas(elements.length, x, y);
+        const id = elements.length;
+        // Initialize element with starting coordinates; x2/y2 will be updated on mouse move
+        const newElement = {
+            id,
+            x1: x,
+            y1: y,
+            x2: x,
+            y2: y,
+            shape: shapeType,
+            roughElement: undefined,
+        };
         dispatch({ type: "ADD_ELEMENT", element: newElement });
     };
 
     const handleMouseMove = (e: React.MouseEvent) => {
         const { x, y } = getScaledCoordinates(e.clientX, e.clientY);
         const lastElement = elements[elements.length - 1];
+        if (!lastElement) return;
         dispatch({ type: "UPDATE_ELEMENT", id: lastElement.id, updates: { x2: x, y2: y } });
     };
 
